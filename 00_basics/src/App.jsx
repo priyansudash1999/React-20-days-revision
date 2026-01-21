9894896f099f374de80e687877772013ea3b4f51
priyansudash1999 <1999priyansudash@gmail.com>
2026-01-21 19:57:10 +0530

    3rd challenge

--- 00_basics/src/App.jsx
+++ 00_basics/src/App.jsx
@@ -3,13 +3,12 @@ import Greet from "./components/Greet";
 import MainComponents from "./components/MainComponents";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <MainComponents />
-      <Footer />
+      <WelcomeMessage />
     </div>
   );
 };
